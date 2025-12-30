aa21ebdbbb7e69fc4a59196f6fcaa17283762de3
leeotus <leeotus@163.com>
2025-12-30 17:44:01 +0800

    format & add comments & add function to close socket

--- include/network/net/lssvc_event.h
+++ include/network/net/lssvc_event.h
@@ -1,29 +1,31 @@
 #ifndef __LSSVC_EVENT_H__
 #define __LSSVC_EVENT_H__
 
+#include <memory>
 #include <string>
 #include <sys/epoll.h>
-#include <memory>
 
 namespace lssvc::network {
 
-// @note currently, we only support epoll. (others (select and poll) are not considered)
+// @note currently, we only support epoll. (others (select and poll) are not
+// considered)
 // @todo io_uring may be implemented in the future
 
 class LSSEventLoop;
-constexpr int kEventRead = (EPOLLIN | EPOLLPRI | EPOLLET);  // use ET
+constexpr int kEventRead = (EPOLLIN | EPOLLPRI | EPOLLET); // use ET
 constexpr int kEventWrite = (EPOLLOUT | EPOLLET);
 
 class LSSEvent : public std::enable_shared_from_this<LSSEvent> {
   friend class LSSEventLoop;
+
 public:
   LSSEvent();
 
   LSSEvent(LSSEventLoop *loop);
 
   /**
-   * @brief when the server accept a client, we get the fd of a client in this 'EventLoop',
-   * then package this client(with its fd) into a Event object
+   * @brief when the server accept a client, we get the fd of a client in this
+   * 'EventLoop', then package this client(with its fd) into a Event object
    */
   LSSEvent(LSSEventLoop *loop, int fd);
 
@@ -44,14 +46,19 @@ public:
   bool enableWriting(bool en);
   bool enableReading(bool en);
 
+  // @brief get the fd value
   int getFd() const;
 
+  // @brief close this event object/socket
+  void close();
+
 protected:
-  LSSEventLoop *loop_;   // the corresponding loop(server) holding this event(client)
+  LSSEventLoop
+      *loop_; // the corresponding loop(server) holding this event(client)
   int fd_;    // file description for epoll
   int event_;
 };
 
-}   // namespace lssvc::network
+} // namespace lssvc::network
 
 #endif
--- src/network/net/lssvc_event.cpp
+++ src/network/net/lssvc_event.cpp
@@ -9,14 +9,10 @@ LSSEvent::LSSEvent() : loop_(nullptr), fd_(-1), event_(0) {}
 
 LSSEvent::LSSEvent(LSSEventLoop *loop) : loop_(loop) {}
 
-LSSEvent::LSSEvent(LSSEventLoop *loop, int fd) : loop_(loop), fd_(fd), event_(0) {}
+LSSEvent::LSSEvent(LSSEventLoop *loop, int fd)
+    : loop_(loop), fd_(fd), event_(0) {}
 
-LSSEvent::~LSSEvent() {
-  if (fd_ > 0) {
-    ::close(fd_);
-    fd_ = -1;
-  }
-}
+LSSEvent::~LSSEvent() { close(); }
 
 // @note the reason why we need to hold a member variable of EventLoop(i.e,
 // loop_) is because we can't alter a Event's flag (like from kEventRead to
@@ -32,3 +28,10 @@ bool LSSEvent::enableWriting(bool en) {
 }
 
 int LSSEvent::getFd() const { return fd_; }
+
+void lssvc::network::LSSEvent::close() {
+  if (fd_ > 1) {
+    ::close(fd_);
+    fd_ = -1;
+  }
+}
